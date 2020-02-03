80c0efd16453cc4a50facd02ca5678b968513296
Eduardo Czpla <edu.czpla@gmail.com>
2020-02-02 22:23:54 -0300

    Upload

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido por<strong> Eduardo <span className="text-danger">Czpla</span></strong>
+        </span>
+    </footer>
